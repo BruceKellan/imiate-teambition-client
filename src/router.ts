e26cbeaf9a1f39fd0a3c323554f365cde09db382
brucekellan <brucekellan@gmail.com>
2019-04-14 23:12:17 +0800

    Synchronization progress By Linying

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,25 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Index from './views/containers/Index.vue';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+  linkActiveClass: 'active',
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: '/login',
+      // TODO
+      component: Index,
+      meta: {
+        login: true,
+      },
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/',
+      component: Index,
     },
   ],
 });
